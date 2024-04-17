75f7fdf2cb4d14ea934ca2d9acd21c5680949ae6
YouSafe <yelagabani@hotmail.com>
2024-04-17 03:46:26 +0200

    Add board without logic

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ChessgroundBoard from '@/components/ChessgroundBoard.vue'
+</script>
 
 <template>
   <header></header>
 
-  <main></main>
+  <div class="flex flex-wrap w-full h-full container-size">
+    <ChessgroundBoard></ChessgroundBoard>
+    <aside></aside>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+}
+</style>
+
+<style scoped>
+.container-size {
+  container-type: size;
+}
+</style>
---
+++ src/components/ChessgroundBoard.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { Chessground } from 'chessground'
+import type { Api } from 'chessground/api'
+import { onMounted, ref } from 'vue'
+
+import '../assets/chessground.base.css'
+import '../assets/chessground.coords.css'
+import '../assets/chessground.brown.css'
+import '../assets/chessground.cburnett.css'
+
+const board = ref<HTMLElement | null>(null)
+const chessground = ref<Api | null>(null)
+
+onMounted(() => {
+  if (board.value === null) {
+    throw new Error('Failed to mount board')
+  }
+
+  const config = {}
+  chessground.value = Chessground(board.value, config)
+})
+</script>
+
+<template>
+  <div ref="board" id="board"></div>
+</template>
+
+<style>
+#board {
+  width: 100cqmin;
+  height: 100cqmin;
+}
+</style>
