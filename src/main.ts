edc1380b63199a4cb75ad578d5214873929536fe
YouSafe <yelagabani@hotmail.com>
2025-08-07 22:33:49 +0200

    Auto reload on service worker registration

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,20 @@ import './assets/styles.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+    onRegisteredSW(_swUrl: string, _registration: ServiceWorkerRegistration | undefined) {
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+            if ((window as any)._hasReloadedOnce) return
+            (window as any)._hasReloadedOnce = true
+            console.info("Reload page after registering service worker")
+            window.location.reload()
+        })
+    },
+    onRegisterError(error: any) {
+        console.error('Service worker registration failed:', error)
+    }
+})
+
 createApp(App).mount('#app')
